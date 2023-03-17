e799a5fd90261dc80501da44350c22544abc9cf6
seif ayman <seifelkomy686@gmail.com>
2023-03-16 17:16:52 -0700

    fix task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char arr[]="\"Programming is like building a multilingual puzzle";
+char arr[] = "\"Programming is like building a multilingual puzzle";
 puts(arr);
 return (0);
 }
