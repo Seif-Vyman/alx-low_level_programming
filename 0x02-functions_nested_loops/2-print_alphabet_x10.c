1fbaf56a8477bd5863aff11df6045889b7a302c7
seif ayman <seifelkomy686@gmail.com>
2023-03-21 16:52:45 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet x10 utilizes on tthe _putchar function to print
+ *      the alphabet a-z x10
+ */
+
+void print_alphabet(void)
+{
+	int i, j;
+
+	for (j = 1; j <= 10; j++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+			_putchar(i);
+		_putchar('\n');
+	}
+}
