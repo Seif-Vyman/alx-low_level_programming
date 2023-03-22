aa3b6d158b617d2465d2533a6638d326072d18e6
seif ayman <seifelkomy686@gmail.com>
2023-03-22 12:04:03 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * void jack_bauer(void) - a void function print all minute in the day
+ * @void:
+ *
+ * no return value :
+ */
+
+void jack_bauer(void)
+{
+	int hours, minutes;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (minutes = 0; minutes <= 59; minutes++)
+		{
+			putchar('0' + hours / 10);
+			putchar('0' + hours % 10);
+			putchar(':');
+			putchar('0' + minutes / 10);
+			putchar('0' + minutes % 10);
+			putchar('\n');
+		}
+	}
+}
