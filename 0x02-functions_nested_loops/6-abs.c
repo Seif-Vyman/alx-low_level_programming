ffb009942ae4519479ea71e2c6579e923bd52845
seif ayman <seifelkomy686@gmail.com>
2023-03-22 07:33:01 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - take a number c and return the abslute of c
+ *  @c: takes input from other functions.
+ *
+ * Return: the abslute of c
+ */
+
+int _abs(int c)
+{
+	int i;
+
+	i = abs(c);
+
+	return (i);
+}
