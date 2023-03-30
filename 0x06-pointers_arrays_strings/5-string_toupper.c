8298cc57a4d06ed40aeb4f681ce8f085e9b092d7
seif ayman <seifelkomy686@gmail.com>
2023-03-30 12:46:24 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* string_toupper - print a  concatenates two strings
+* @s: pointer of string
+* Return: return string of dest and src
+*/
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; i != '\0'; ++i)
+	{
+		if ('z' >= s[i] && s[i] >= 'a')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
