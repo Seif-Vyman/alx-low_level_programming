ba33d216cc9c45e87274de097eeb108288fc9351
seif ayman <seifelkomy686@gmail.com>
2023-04-11 11:19:35 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ * @argc: number of arguments
+ * @argv: vector which have arguments
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
