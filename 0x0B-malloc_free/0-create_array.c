0c1dbb5ff82aeeb1f8773405115f85ee577aa631
seif ayman <seifelkomy686@gmail.com>
2023-04-11 16:55:37 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+* create_array - print a  concatenates two strings
+* @size: pointer of string
+* @c: number of bytes
+*/
+char *create_array(unsigned int size, char c)
+{
+	char x;
+
+	x = malloc(size);
+	if (size == '\0' || x == 0)
+		return (0);
+	while (size--)
+		x[size] = c;
+	return (x);
+}
