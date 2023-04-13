489dedfb45df3de002c4e0507c54b43b4c7562da
seif ayman <seifelkomy686@gmail.com>
2023-04-13 15:13:45 -0700

    task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+* array_range - print a  concatenates two strings
+* @min: pointer of string
+* @max: pointer of string2
+* Return: return copy for array
+*/
+int *array_range(int min, int max)
+{
+	int *x, i;
+
+	if (min > max)
+		return (NULL);
+	x = malloc((max - min + 1) * sizeof(int) + 1);
+	if (x == 0)
+		return (NULL);
+	for (i = 0; min <= max; min++, i++)
+		x[i] = min;
+	x[i] = '\0';
+	return (x);
+}
