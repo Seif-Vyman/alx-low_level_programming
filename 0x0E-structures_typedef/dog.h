2143842fd1c9d6941115340a051d6a0b29c3f14f
seif ayman <seifelkomy686@gmail.com>
2023-04-17 16:13:31 -0700

    fix task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * struct dog - this stract for dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: dog's friend
+ *
+ * Description: this struct make for dog information like name
+ */
 struct dog
 {
 	char *name;
