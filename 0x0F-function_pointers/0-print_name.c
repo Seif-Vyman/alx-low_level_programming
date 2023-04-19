416f279e42ec707e596be57900a1d8c5a7bd4a7e
seif ayman <seifelkomy686@gmail.com>
2023-04-19 11:11:15 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+*  print_name - function fills the first n bytes of the memory area
+* @name: pointer of char
+* @f: ponter of function
+* _putchar - function that print chars
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
