985a48ace9a1eeb2f735211cb3a13c72d987da08
seif ayman <seifelkomy686@gmail.com>
2023-04-19 12:51:47 -0700

    task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+*  int_index - function fills the first n bytes of the memory area
+* @array: pointer of char
+* @size: ponter of function
+* @cmp: pointer of function
+* Return: return the index
+* _putchar - function that print chars
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; ++i)
+	{
+		if((*cmp)(*array++))
+		return (i);
+	}
+	return (-1);
+}
