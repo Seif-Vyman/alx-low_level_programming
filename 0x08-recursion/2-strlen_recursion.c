527fb8c330101cee78cf2f1340f34808009c61ed
seif ayman <seifelkomy686@gmail.com>
2023-06-07 11:56:33 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function print words by recursion
+ * @s: pointer to word
+ * Return: counter of length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 +  _strlen_recursion(s+1));
+}
