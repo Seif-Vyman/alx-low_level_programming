8fd3db2a43a10c89bf1c2866f5b4ece9e9c14ef3
seif ayman <seifelkomy686@gmail.com>
2023-06-12 16:46:52 -0700

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - function make dynamic array
+ * @size: size of array
+ * @c: char will write
+ * Return: return pointer to array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	unsigned int i = 0;
+
+	if (size)
+	{
+		s = malloc(sizeof(char) * (size + 1));
+		if (s)
+		{
+			while (i < size)
+			{
+				s[i] = c;
+				i++;
+			}
+			s[i] = '\0';
+			return (s);
+		}
+		else
+			return (NULL);
+	}
+	else
+	return (NULL);
+}
