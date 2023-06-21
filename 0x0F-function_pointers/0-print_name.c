6eb7e932ff7449c34211005f39754727d7026727
seif ayman <seifelkomy686@gmail.com>
2023-06-21 14:32:36 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function use pointer to print
+ * @name: pointer to name
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
