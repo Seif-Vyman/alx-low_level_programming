9899b866c23c367e28863948a8aeef6d3d8b0022
seif ayman <seifelkomy686@gmail.com>
2023-06-21 14:54:14 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function use pointer to print
+ * @array: pointer to name
+ * @size: pointer to function
+ * @action: pointer to function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && size && action)
+	{
+		long unsigned int i;
+
+		for (i = 0; i < size; i++)
+		{
+			(*action)(array[i]);
+		}
+	}
+}
